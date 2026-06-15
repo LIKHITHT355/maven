0c1cbe3d9c13966ab645fdd544ad1dd258ef4834
LIKHITHT355 <llikilaki@gmail.com>
2026-06-15 21:42:06 +0530

    Initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int a = 5;
+        int b =5;
+        System.out.println( "a+b="+(a+b));
+    }
+}
